9fafde8a0155a3bb4b49248d0599742cd18d9d19
jklip506 <96444138+jklip506@users.noreply.github.com>
2023-12-02 15:01:41 -0600

    Updated feedbacks card for smaller iphones

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -16,16 +16,16 @@ const FeedbackCard = ({
 }) => (
   <motion.div
     variants={fadeIn("", "spring", index * 0.5, 0.75)}
-    className='bg-black-200 p-10 rounded-3xl xs:w-[420px] w-full flex flex-col justify-between h-[400px]' // Ensuring full height
+    className='bg-black-200 p-10 rounded-3xl xs:w-[420px] w-full flex flex-col justify-between xs:h-[400px]' // Ensuring full height
   >
     <div className='h-[60%]'> {/* Fixed height for testimonial section */}
       <p className='text-white font-black text-[48px]'>"</p>
       <p className='text-white tracking-wider text-[18px] overflow-hidden'>{testimonial}</p>
     </div>
 
-    <div className='mt-auto'> {/* Push to bottom */}
+    <div className='mt-7 xs:mt-auto'> {/* Push to bottom */}
       <div className='flex justify-between items-center gap-1'>
-        <div className='flex flex-col'>
+        <div className='flex flex-col '>
           <p className='text-white font-medium text-[16px]'>
             <span className='blue-text-gradient'>@</span> {name}
           </p>
